c7cf12bc5928ab96041b167994f00b20c52bb09c
xujingw1 <xujingwei94@gmail.com>
2017-02-05 14:13:50 +0800

    bug fix and clean up

--- core/client/controllers/navbar.client.controller.js
+++ core/client/controllers/navbar.client.controller.js
@@ -5,10 +5,18 @@
 angular.module('navbar').controller('NavbarController',
 ['$scope','Authentication','$location', function ($scope, Authentication, $location) {
     $scope.authentication = Authentication;
-    if (!Authentication) {
-        $scope.account = "icon-user-plus";
-    } else {
-        $scope.account = Authentication.
+    $scope.go = function (path) {
+        if ( path == "/wallet") {
+            console.log(Authentication.user);
+            path = path + "/" + Authentication.user._id;
+        }
+        $location.path(path);
     }
-}
-])
+/**
+ *  if (!Authentication) {
+ *      $scope.account = "icon-user-plus";
+ *  } else {
+ *      $scope.account = Authentication.user
+ *  }
+ */
+}])
